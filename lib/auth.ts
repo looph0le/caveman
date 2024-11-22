6f010e39ba12ac630d9d9960326f83222206b4e8
looph0le <mitanshpanchal@gmail.com>
2024-11-23 02:24:44 +0530

    Fix production

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,7 @@
 import { NextAuthOptions, User, getServerSession } from "next-auth";
-import { useSession } from 'next-auth/react';
 import { redirect } from 'next/navigation';
 
-import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from 'next-auth/providers/google'
-import GithubProvider from 'next-auth/providers/github'
 
 export const authConfig: NextAuthOptions = {
   providers: [
@@ -13,7 +10,7 @@ export const authConfig: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     })
   ],
-  secret: process.env.AUTH_SECRET
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
 export async function loginIsRequiredServer() {
