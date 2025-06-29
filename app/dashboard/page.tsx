7b38d050d932546306e1543be385b74666a763eb
looph0le <mitanshpanchal@gmail.com>
2025-06-29 13:03:41 +0530

    add key to genai

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -25,7 +25,7 @@ export default async function Dashboard() {
     redirect("/");
   }
 
-  const ai = new GoogleGenAI({});
+  const ai = new GoogleGenAI({apiKey: process.env.GOOGLE_GENAI_API_KEY});
   const aiResponse = await ai.models.generateContent({
     model: "gemini-1.5-flash",
     contents: "Give new motivational quote for gym, make them 2 liner, response with only 1 quote each time. use name of client" + session.user.name + " in the quote.",
