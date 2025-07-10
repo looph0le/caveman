24ff4af0033f2480df82bc83cb12cd4ba9e9d32d
Mitansh Panchal <71015415+looph0le@users.noreply.github.com>
2025-07-10 16:42:51 +0000

    loader

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function Loading() {
+    return (
+        <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-black/50 backdrop-blur-sm transition-all">
+            <div className="w-10 h-10 border-4 border-gray-500 border-t-transparent rounded-full animate-spin"></div>
+        </div>
+    )
+}
